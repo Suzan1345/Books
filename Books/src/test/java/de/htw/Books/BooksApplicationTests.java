cc3cc39fd560f67b105988bd45b193ecde333e68
Suzan1345 <Suzan.Korkmaz@Student.HTW-Berlin.de>
2025-05-24 16:37:20 +0200

    .

---
+++ Books/src/test/java/de/htw/Books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
